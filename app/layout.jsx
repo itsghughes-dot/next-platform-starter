a51a77d49afed921a3c2f3f6c70e6730ba250bca
Greg Hughes <greghughes@Gregs-MacBook-Pro.local>
2026-03-03 23:14:19 +0000

    Add Google Analytics 4 (G-6F5HPEH1S4)

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,25 +1,35 @@
 import '../styles/globals.css';
 
 export const metadata = {
-    title: {
-        template: '%s | Worst Eleven',
-        default: 'Worst Eleven'
-    }
+  title: {
+    template: '%s | Worst Eleven',
+    default: 'Worst Eleven',
+  },
 };
 
 export default function RootLayout({ children }) {
-    return (
-        <html lang="en">
-            <head>
-                <link rel="icon" href="/favicon.svg" sizes="any" />
-                <link
-                    href="https://fonts.googleapis.com/css2?family=Bebas+Neue&family=DM+Sans:wght@300;400;500;600&family=DM+Mono:wght@400;500&display=swap"
-                    rel="stylesheet"
-                />
-            </head>
-            <body>
-                {children}
-            </body>
-        </html>
-    );
+  return (
+    <html lang="en">
+      <head>
+        <link rel="icon" href="/favicon.svg" sizes="any" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Bebas+Neue&family=DM+Sans:wght@300;400;500;600&family=DM+Mono:wght@400;500&display=swap"
+          rel="stylesheet"
+        />
+        {/* Google Analytics 4 */}
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-6F5HPEH1S4"></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments)}
+              gtag('js', new Date());
+              gtag('config', 'G-6F5HPEH1S4');
+            `,
+          }}
+        />
+      </head>
+      <body>{children}</body>
+    </html>
+  );
 }
